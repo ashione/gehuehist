50ac1c5fbd8065fa6007134392f85fccc24b9d05
whenwood <whenwood@whenwood-MacBook-Pro.local>
2016-01-23 17:25:35 +0800

    add gravity

---
+++ gravity/gravity.h
@@ -0,0 +1,17 @@
+#ifndef GRAVITY_H
+#define GRAVITY_H
+
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+struct Point {
+    double x;
+    double y;
+    Point(double a, double b) : x(a), y(b){};
+};
+
+void gravity(vector<double>&,vector<double>&);
+
+#endif
